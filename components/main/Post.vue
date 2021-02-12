9c06f362f5d942ed9b8050a0deab3d5884bed217
dm1annem <300225@mail.ru>
2021-02-12 11:03:24 +0300

    добавил компонент post

---
+++ components/main/Post.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="post">
+
+  </section>
+</template>
+
+<script>
+export default {
+  
+   
+};
+</script>
+
+<style lang="scss" scoped>
+.post{
+  height: 200px;
+  border: 1px solid black;
+  margin-bottom: 2rem;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,24 @@
 <template>
+    <el-row type="flex" justify="center">
+        <el-col :xs="24" :sm="18" :md="16" :lg="14" :xl="12">
+            <app-post
+            v-for="post in 5"
+            :key="post"
+            />
+
+        </el-col>
+    </el-row>
   
 </template>
 
 <script>
-export default {}
+import AppPost from '@/components/main/Post.vue'
+export default {
+    components:{
+        AppPost
+    }
+
+}
 </script>
 
 <style>
