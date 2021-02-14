3e52fb8bcd10fbbeed55e61657749db42003e231
Нуфтанчики <300225@mail.ru>
2021-02-14 18:45:08 +0300

    created admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="wrap">
+
+        <el-container :style="{height: '100%'}">
+            <el-aside width="200px">Aside</el-aside>
+            <el-main>
+                <nuxt/>
+            </el-main>
+        </el-container>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style leng="scss" scoped>
+.wrap{
+    width: 100%;
+    height: 100vh;
+}
+
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Привет из админки</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'admin',
+
+}
+</script>
