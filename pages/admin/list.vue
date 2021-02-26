5de657682686096babad83540f61deba9b2f0fcc
Нуфтанчики <300225@mail.ru>
2021-02-27 00:30:35 +0300

    created posts list

--- pages/admin/list.vue
+++ pages/admin/list.vue
@@ -1,10 +1,84 @@
 <template>
-    <h1>list</h1>
+    <el-table
+    :data="posts"
+    style="width: 100%">
+
+    <el-table-column
+    prop="title"
+    label="Название"
+    />
+    <el-table-column label="Дата">
+      <template slot-scope="{row: {date}}">
+        <i class="el-icon-time"></i>
+        <span style="margin-left: 10px">{{ new Date(date).toLocaleString()  }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column
+    prop="veiws"
+    label="Просмотры">
+      <template slot-scope="{row: {veiws}}">
+        <i class="el-icon-view"></i>
+        <span style="margin-left: 10px">{{ veiws  }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+    prop="comments"
+    label="Коммментарии">
+      <template slot-scope="{row: {comments}}">
+        <i class="el-icon-chat-square"></i>
+        <span style="margin-left: 10px">{{ comments.length  }}</span>
+      </template>
+    </el-table-column>
+
+    <el-table-column
+      label="Действия">
+      <template slot-scope="{row}">
+        <el-button
+          icon="el-icon-edit"
+          circle
+          type="primary"
+          @click="open(row._id)"></el-button>
+
+        <el-button
+          icon="el-icon-delete"
+          circle
+          type="danger"
+          @click="remove(row._id)"></el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
+
 
 <script>
 export default {
     layout: 'admin',
     middleware: ['admin-auth'],
+    data(){
+        return{
+            
+        }
+    },
+
+    async asyncData({store}) {
+        const posts = await store.dispatch('post/fitchAdminPosts')
+        
+        return {posts}
+    },
+    
+    methods: {
+      open(id){
+        console.log('open',id)
+      },
+
+      remove(id){
+        console.log('remove',id)
+      }
+    }
+
+
+
+     
 }
 </script>
---
+++ store/post.js
@@ -0,0 +1,17 @@
+import 'core-js/es/promise'
+
+export const actions = {
+    async fitchAdminPosts(){
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve(
+                    [
+                        {title: 'Post 1', date: new Date(), veiws: 34, comments: [1, 2], _id: Math.random()},
+                        {title: 'Post 2', date: new Date(), veiws: 45, comments: [1, 2, 3], _id: Math.random()}
+                    ]
+                )
+            }, 1000)
+        })
+    }
+}
+
