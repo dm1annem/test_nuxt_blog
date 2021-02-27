9136007d96bdfdc7f450c021e2e196ed8cd64f39
Нуфтанчики <300225@mail.ru>
2021-02-27 23:29:24 +0300

    создал форму для изменения постов

--- pages/admin/post/_id.vue
+++ pages/admin/post/_id.vue
@@ -1,10 +1,47 @@
 <template>
-    <div>
-        <el-breadcrumb separator="/">
+    <div class="page-wrap">
+        <el-breadcrumb separator="/" class="mb">
             <el-breadcrumb-item to="/admin/list/">Список постров</el-breadcrumb-item>
-            <el-breadcrumb-item>Пост {{ $route.params.id }}</el-breadcrumb-item>
+            <el-breadcrumb-item>Пост {{ post.title }}</el-breadcrumb-item>
         </el-breadcrumb>
         <h3></h3>
+
+        <el-form 
+        :model="controls" 
+        :rules="rules" 
+        ref="form"
+        @submit.native.prevent="onSubmit"
+        >
+                <el-form-item label="Текст в формате .md или .html" prop="text">
+                    <el-input 
+                    type="textarea"
+                    resize="none"
+                    v-model="controls.text"
+                    :rows="10"
+                />
+                </el-form-item>
+                
+                <div class="mb">
+                    <small class="mr">
+                        <i class="el-icon-time"></i>
+                        <span>{{ new Date(post.date).toLocaleString()  }}</span>
+                    </small>
+
+                    <small>
+                        <i class="el-icon-view"></i>
+                        <span>{{ post.veiws}}</span>
+                    </small>
+                </div>
+        
+                <el-form-item >
+                    <el-button 
+                    type="primary"
+                    native-type="submit"
+                    round
+                    :loading="loading"
+                    >Обновить</el-button>
+                </el-form-item>
+        </el-form>
     </div>
 
 </template>
@@ -13,5 +50,70 @@
 export default {
     layout: 'admin',
     middleware: ['admin-auth'],
+    head(){
+        return{
+            title: `Пост - ${this.post.title}`
+        }
+    },
+
+    validate({params}){
+        return Boolean(params.id)
+    },
+
+    async asyncData({ store,  params}) {
+        const post = await store.dispatch('post/fitchAdmynById', params.id)
+        return {post}
+    },
+
+    data() {
+        return{
+            loading: false,
+
+            controls:{
+              text: '',
+              
+            },
+
+            rules:{
+                text:[
+                    {required: true, message: 'Поле не должно быть пустым', trigger: 'blur' }
+                ],
+
+            }
+        }
+    },
+
+    methods: {
+        onSubmit(){
+            this.$refs.form.validate(async valid => {
+                this.loading = true
+
+                const formData ={
+                    text: this.controls.text,
+                    id: this.post._id
+                }
+
+                try{
+                    await this.$store.dispatch('post/update', formData)
+                    this.message.success('Пост обновлён')
+                    this.loading = false
+                }catch(e){
+                    this.loading = false
+                }
+                
+            })
+        }
+    }
 }
 </script>
+
+
+<style lang="scss" scoped>
+.page-wrap{
+    width: 600px;
+}
+
+.mr{
+    margin-right: 2rem;
+}
+</style>
--- store/post.js
+++ store/post.js
@@ -1,21 +1,34 @@
 import 'core-js/es/promise'
 
+
+const posts = [
+    {title: 'Post 1', date: new Date(), veiws: 34, comments: [1, 2], _id: 'id1'},
+    {title: 'Post 2', date: new Date(), veiws: 45, comments: [1, 2, 3], _id: 'id2'}
+]
+
 export const actions = {
     async fitchAdminPosts(){
         return await new Promise(resolve => {
             setTimeout(() => {
-                resolve(
-                    [
-                        {title: 'Post 1', date: new Date(), veiws: 34, comments: [1, 2], _id: Math.random()},
-                        {title: 'Post 2', date: new Date(), veiws: 45, comments: [1, 2, 3], _id: Math.random()}
-                    ]
-                )
+                resolve(posts)
             }, 1000)
         })
     },
 
     async remove({}, id){
         
+    },
+
+    async update({}, {id, text}){
+
+    },
+
+    async fitchAdmynById({}, id){
+        return await new Promise(resolve => {
+            setTimeout(() => {
+                resolve(posts.find(p => p._id === id))
+            }, 1000)
+        })
     }
 }
 
